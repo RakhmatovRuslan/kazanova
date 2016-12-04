f44d6259a6bb8eebbd6c7f49c8b3a6cbd61f204b
Rakhmatov Ruslan <rakhmatovruslan@gmail.com>
2016-12-04 17:22:38 +0900

    Test is done

--- src/test/java/WebFrameworkRulesTest.java
+++ src/test/java/WebFrameworkRulesTest.java
@@ -1,4 +1,6 @@
 import com.javacodegeeks.drools.LibrariesRepository;
+import com.javacodegeeks.drools.enums.Features;
+import com.javacodegeeks.drools.enums.WebApplicationType;
 import com.javacodegeeks.drools.frameworks.Framework;
 import com.javacodegeeks.drools.tasks.WebFrameworkTask;
 import org.junit.Assert;
@@ -17,6 +19,7 @@ public class WebFrameworkRulesTest {
 
     List<Framework> webFrameworkList;
     WebFrameworkTask webFrameworkTask;
+    WebFrameworkTask webFrameworkTask2;
     KieSession kSession;
 
     @Before
@@ -24,14 +27,19 @@ public class WebFrameworkRulesTest {
         loadRuleEngine();
         webFrameworkList = LibrariesRepository.getInstance().getWebFrameworks();
         webFrameworkTask = new WebFrameworkTask();
+        webFrameworkTask2 = new WebFrameworkTask();
+        webFrameworkTask2.setFeaturePriority(Features.EASE_OF_USE);
+        webFrameworkTask2.setWebApplicationType(WebApplicationType.COMPONENT_BASED);
     }
 
     @Test
     public void webFrameworkTaskTest(){
-        kSession.setGlobal("webFrameworkList",webFrameworkList);
+        kSession.setGlobal("webFrameworkList", webFrameworkList);
         kSession.insert(webFrameworkTask);
+        kSession.insert(webFrameworkTask2);
         kSession.fireAllRules();
         Assert.assertEquals("Grails", webFrameworkTask.getFramework().getName());
+        Assert.assertEquals("JSF",webFrameworkTask2.getFramework().getName());
     }
 
     public void loadRuleEngine(){
