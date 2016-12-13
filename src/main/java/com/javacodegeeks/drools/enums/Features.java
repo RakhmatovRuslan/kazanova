c062725f2d0697c7cd9f5c9956a0842711e05504
rakhmatovruslan@gmail.com <rakhmatovruslan@gmail.com>
2016-12-14 02:40:28 +0900

    Merge branch 'ruslan' of https://github.com/RakhmatovRuslan/kazanova into ruslan improvements in reasoning for json parser library

--- src/main/java/com/javacodegeeks/drools/enums/Features.java
+++ src/main/java/com/javacodegeeks/drools/enums/Features.java
@@ -23,4 +23,12 @@ public enum Features {
     Features(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
